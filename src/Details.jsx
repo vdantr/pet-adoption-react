081c4691fcb7143afc0b40630f51f0dc06a41172
unknown <veselin.trifonov.tt@gmail.com>
2023-02-06 21:56:41 +0100

    finished

---
+++ src/Details.jsx
@@ -0,0 +1,71 @@
+import { useState, useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import AdoptedPetContext from "./AdoptedPetContext";
+import { useQuery } from "@tanstack/react-query";
+import Carousel from "./Carousel";
+import fetchPet from "./fetchPet";
+import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
+
+const Details = () => {
+    const [showModal, setShowModel] = useState(false)
+    const navigate = useNavigate()
+    
+    // eslint-disable-next-line no-unused-vars
+    const [_, setAdoptedPet] = useContext(AdoptedPetContext)
+    const {id} = useParams()
+    const results = useQuery(["details", id], fetchPet)
+
+    if (results.isLoading) {
+        return (
+            <div className="loading-pane">
+                <h2 className="loader">🌀</h2>
+            </div>
+        )
+    }
+
+    const pet = results.data.pets[0]
+
+    return (
+        <div className="details">
+        <Carousel images={pet.images} />
+            <div>
+                <h1>{pet.name}</h1>
+                <h2>
+                    {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
+                    <button onClick={() => setShowModel(true)}>Adopt {pet.name}</button>
+                    <p>{pet.description}</p>
+                    {   showModal ? (
+                            <Modal>
+                                <div>
+                                    <h1>Would you like to adopt {pet.name}?</h1>
+                                    <div className="buttons">
+                                        <button 
+                                            onClick={() => {
+                                                setAdoptedPet(pet)
+                                                navigate("/")
+                                            }}
+                                        >
+                                            Yes
+                                        </button>
+                                        <button onClick={() => setShowModel(false)}>No</button>
+                                    </div>
+                                </div>
+                            </Modal>
+                        ) : null
+                    }
+                </h2>
+            </div>
+        </div>
+    );
+  };
+
+  function DetailsErrorBoundary() {
+    return (
+        <ErrorBoundary>
+            <Details />
+        </ErrorBoundary>
+    )
+  }
+  
+  export default DetailsErrorBoundary;
